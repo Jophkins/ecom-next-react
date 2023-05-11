25ff2ba95125d32b219130b5e9c168133df16343
serhiilaptop <sergebrew@gmail.com>
2023-05-11 01:42:43 -0600

    admin-dashboard basic design

---
+++ admin/pages/index.js
@@ -0,0 +1,23 @@
+import Layout from "@/components/Layout";
+import {useSession} from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  const {data: session} = useSession();
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-200 gap-1 text-black rounded-lg overflow-hidden">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={session?.user?.image} alt="" className="w-6 h-6" />
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
+        </div>
+      </div>
+    </Layout>
+  )
+}
